93b7ded9286f512546407639eb7d3d22645f1b41
StretchyRhombus <66787780+salohcin-n@users.noreply.github.com>
2024-03-01 20:21:11 -0400

    New scraper
    -Added a new scraper for another section under Fruits & Vegetables -Packaged salad and dressings.
    -Need to add a way to iterate through scrapers to run, rather than calling and running in the main method.

--- grocery-scraper/.venv/scraper.py
+++ grocery-scraper/.venv/scraper.py
@@ -1,6 +1,7 @@
 import time
 from models.fruits_vegetables.fresh_vegetables import FreshVeggieExtractor
 from models.fruits_vegetables.fresh_fruits import FreshFruitExtractor
+from models.fruits_vegetables.packaged_salad import PackagedSaladAndDressingsExtractor
 from data.data import printData
 
 def main():
@@ -8,10 +9,13 @@ def main():
         # Create an instance of a model
         veggie_extractor = FreshVeggieExtractor()
         fruit_extractor = FreshFruitExtractor()
+        packaged_salad_extractor = PackagedSaladAndDressingsExtractor()
+
 
         # Run the extraction process
         veggie_extractor.run()
         fruit_extractor.run()
+        packaged_salad_extractor.run()
 
         # Uncomment this line to print data to the console -for testing
         # printData()
