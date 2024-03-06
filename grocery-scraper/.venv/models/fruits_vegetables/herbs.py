5527acb762538b504af672d3420aff4ae4630f4b
StretchyRhombus <66787780+salohcin-n@users.noreply.github.com>
2024-03-06 19:22:14 -0400

    updates
    Scrapers now iterating and running.
    Added herbs scraper- not functional

---
+++ grocery-scraper/.venv/models/fruits_vegetables/herbs.py
@@ -0,0 +1,42 @@
+from selenium.webdriver.common.by import By
+from models.base import PriceExtractor, TitleExtractor, DisplayData, DataExtractor
+from util.functions import initialize_driver, web_functions, auto_scroll, last_page
+from data.data import saveData
+
+# Global variables
+page_url = 'https://www.atlanticsuperstore.ca/food/fruits-vegetables/herbs/c/28197'
+
+# Main class for Fresh Vegetables section
+class HerbExtractor:
+    # Method to self initialize the driver - Currently running from scraper.py
+    # def __init__(self):
+    #     self.driver = initialize_driver(page_url)
+
+    def run(self):
+        try:
+            # Initialize the WebDriver
+            self.driver = initialize_driver(page_url)
+
+            # Initializing lists and display object
+            herb_data = []
+
+            # Open a webpage and perform actions
+            web_functions(self.driver)
+
+            # Create instances of PriceExtractor and TitleExtractor and extract data
+            data_extractor = DataExtractor(self.driver)
+
+
+
+
+                # Printing the list from a page if all products are on the same page
+                # print(display.display_prices_and_titles(prices=price_extractor.extract_data(), titles=title_extractor.extract_data()))
+            herb_data.append(data_extractor.extract_data())
+
+            # Save data and Close the browser
+            saveData(herb_data, '''herbs''')
+            self.driver.quit()
+
+        # End of try/except error handling
+        except Exception as e:
+            print(f"Unexpected error with run function - herbs: {str(e)}")
--- grocery-scraper/.venv/scraper.py
+++ grocery-scraper/.venv/scraper.py
@@ -2,19 +2,27 @@ import time
 from models.fruits_vegetables.fresh_vegetables import FreshVeggieExtractor
 from models.fruits_vegetables.fresh_fruits import FreshFruitExtractor
 from models.fruits_vegetables.packaged_salad import PackagedSaladAndDressingsExtractor
+from models.fruits_vegetables.herbs import HerbExtractor
 from data.data import printData
 
 def main():
     try:
-        # Create an instance of a model
-        veggie_extractor = FreshVeggieExtractor()
-        fruit_extractor = FreshFruitExtractor()
-        packaged_salad_extractor = PackagedSaladAndDressingsExtractor()
+        # # Create an instance of a model
+        # herb_extractor = HerbExtractor()
+        #
+        # # Run the extraction process
+        # herb_extractor.run()
 
-        # Run the extraction process
-        veggie_extractor.run()
-        fruit_extractor.run()
-        packaged_salad_extractor.run()
+        # Create instances of the scrapers
+        scrapers = [
+            FreshVeggieExtractor(),
+            FreshFruitExtractor(),
+            PackagedSaladAndDressingsExtractor()
+        ]
+
+        # Run each scraper
+        for scraper in scrapers:
+            scraper.run()
 
         # Check the function for errors before running.
         # Uncomment this line to print data to the console -for testing -may DELETE/DROP tables
