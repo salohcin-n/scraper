2c3240a79d963b5d62c43e43266ad1011efc87a7
StretchyRhombus <66787780+salohcin-n@users.noreply.github.com>
2024-03-10 23:40:06 -0300

    Create chicken_turkey.py

---
+++ grocery-scraper/.venv/models/meat/chicken_turkey.py
@@ -0,0 +1,46 @@
+from selenium.webdriver.common.by import By
+from models.base import PriceExtractor, TitleExtractor, DisplayData, DataExtractor
+from models.types import GroceryType
+from util.functions import initialize_driver, web_functions, auto_scroll, last_page
+from data.data import saveData, saveMeatData
+
+# Global variables
+page_url = 'https://www.atlanticsuperstore.ca/food/meat/chicken-turkey/c/28214'
+
+# Main class for Fresh Fruits section
+class ChickenTurkeyExtractor:
+
+    def run(self):
+        try:
+            # Initialize the WebDriver
+            self.driver = initialize_driver(page_url)
+
+            # Initializing lists and display object
+            chicken_turkey_data = []
+            display = DisplayData()
+
+            # Open a webpage and perform actions
+            web_functions(self.driver)
+
+            # Create instances of PriceExtractor and TitleExtractor and extract data
+            data_extractor = DataExtractor(self.driver)
+
+            if(self.driver.find_element(By.CSS_SELECTOR, 'button[aria-label="Next Page"]')):
+                for x in range(last_page(driver=self.driver)):
+                    chicken_turkey_data.append(data_extractor.extract_data())
+                    nextPageBtn = self.driver.find_element(By.CSS_SELECTOR, 'button[aria-label="Next Page"]')
+                    nextPageBtn.click()
+                    auto_scroll(self.driver)
+
+                saveMeatData(chicken_turkey_data, GroceryType.CHICKEN_TURKEY.value)
+
+            else:
+                # Printing the list from a page if all products are on the same page
+                print(display.display_prices_and_titles(prices=price_extractor.extract_data(), titles=title_extractor.extract_data()))
+
+            # Close the browser
+            self.driver.quit()
+
+        # End of try/except error handling
+        except:
+            print("Unexpected error with run function - fresh_fruits")
