d4e28566b3e7cdfad8395c25e55e377df0a2121d
StretchyRhombus <66787780+salohcin-n@users.noreply.github.com>
2024-03-16 18:35:33 -0300

    GUI
    Added GUI to display data from the produce table after scraping.

--- grocery-scraper/.venv/scraper.py
+++ grocery-scraper/.venv/scraper.py
@@ -1,6 +1,8 @@
 import time
 from util.scraper_run import FRUIT_AND_VEGGIE_SCRAPERS, MEAT_SCRAPERS
+from util.globals import DATABASE_PATH
 from data.data import printData
+from data.gui_display import create_gui
 
 def main():
     try:
@@ -14,6 +16,8 @@ def main():
 
         # Uncomment this line to print data to the console -for testing -may DELETE/DROP tables
         # printData()
+
+        create_gui(DATABASE_PATH)
     except:
         print("Issue with main scraper run")
 
---
+++ grocery-scraper/.venv/util/globals.py
@@ -0,0 +1,2 @@
+# Global variables
+DATABASE_PATH = r'C:\GitHub\scraper\grocery-scraper\.venv\loblaws.db'
